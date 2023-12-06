a5f46fd5147d9c0fc9cdddd2495cc53844e3fb2e
zepor <johnbonnett3@gmail.com>
2023-12-06 01:23:56 -0600

    ACME

--- frontend-container/server.js
+++ frontend-container/server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", function (req, res) {
+// Special handling for ACME challenge route
+app.get("/.well-known/acme-challenge/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Handle all other routes
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
