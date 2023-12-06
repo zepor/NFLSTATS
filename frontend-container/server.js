87b12d257d45b70c4632010e993e07db2b30357f
zepor <johnbonnett3@gmail.com>
2023-12-05 20:28:37 -0600

    wrong server.js dir

--- frontend-container/server.js
+++ frontend-container/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
